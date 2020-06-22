817cf41f370668fcfbdbfde188f11e2c6d473136
Younus <alia.younus@accenture.com>
2020-06-22 17:20:40 -0500

    second commit

--- problems/07-palindrome/07-palindrome.js
+++ problems/07-palindrome/07-palindrome.js
@@ -2,5 +2,6 @@
 Define function isPalindrome(someStr)
 Return true if someStr is a palindrome, otherwise return false */
 function isPalindrome(someStr) {
-  
+
+    
 }
--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -1,6 +1,23 @@
 /* 9. Object Properties
 Define function objectProperties(someObj)
 Print every property and it's value. */
-function objectProperties(someObj) {
+//function objectProperties(someObj){}
+ 
+    function emp(id,name, age, salary){
+    
+        this.id = id
+        this.name= name
+        this.age = age
+        this.salary = salary
+        
+    }
+    
+    
+    let empInfo = new emp('112', 'john', 34, 27000)
+    console.log(empInfo);
+    
+    let empDetail= new emp('113', 'Marry', 29, 500000)
+    console.log(empDetail);
+    
+    
 
-}
